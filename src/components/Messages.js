4653a68298788217faf0cd9797c8ca758ed7667c
Brianna Greene <brianna.greene.designs@gmail.com>
2017-11-02 16:17:12 -0600

    updating messages

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import Message from './Message'
 
-const Messages = ({ }) => {
+const Messages = ({ messages }) => {
+  const messageComponents = messages.map(message => (
+    <Message
+      key={message.id}
+      message={message}
+      toggleSelect={toggleSelect}
+      toggleStar={toggleStar}
+    />
+  ))
 
+  return (
+    <div>
+      {messageComponents}
+    </div>
+  )
 }
 
 export default Messages
