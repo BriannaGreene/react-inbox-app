a07dda49869f776f7f6b2f0b013b2150722570d8
Brianna Greene <brianna.greene.designs@gmail.com>
2017-11-02 20:56:30 -0600

    working on app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Messages from '../components/Messages'
+// import Messages from '../components/Messages'
 import './App.css';
 
 class App extends Component {
@@ -7,17 +7,56 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = { messages: props.messages }
+    console.log('this.state from App.js: ', this.state);
   }
 
   // toggle property function
+  toggleProperty(message, property) {
+    console.log('message from app js: ', message);
+    console.log('property from app js: ', property);
+    this.setState(prevState => {
+      const index = prevState.messages.indexOf(message)
+      return {
+        messages: [
+          ...prevState.messages.slice(0, index),
+          { ...message, [property]: !message[property] },
+          ...prevState.messages.slice(index + 1)
+        ]
+      }
+    })
+  }
 
   // toggle select function
+  toggleSelect(message) {
+    this.toggleProperty(message, 'selected')
+  }
 
   // toggle star function
+  toggleStar(message) {
+    this.toggleProperty(message, 'starred')
+  }
 
   // mark as read function
+  markAsRead() {
+    this.setState(prevState => {
+      return {
+        messages: prevState.messages.map(message => (
+          message.selected ? { ...message, read: true } : message
+        ))
+      }
+    })
+  }
 
   // mark as unread function
+  markAsUnread() {
+    this.setState(prevState => {
+      return {
+        messages: prevState.messages.map(message => (
+          message.selected ? { ...message, read: false } : message
+        ))
+      }
+    })
+  }
 
   // delete messages
 
@@ -30,14 +69,8 @@ class App extends Component {
   // render DOM stuff
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h3>Hello</h3>
       </div>
     );
   }
